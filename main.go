7ede7e22ef5e047b661084f04a101567dcb12aeb
Akmf <763941102@qq.com>
2024-06-21 01:24:27 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"Gohub/bootstrap"
+	Config "Gohub/config"
+	"Gohub/pkg/config"
+	"flag"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	Config.Initialize()
+}
+
 func main() {
-	// 初始化 Gin 实例
-	r := gin.Default()
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
+	// new 一个 Gin Engine 实例
+	router := gin.New()
 
 	// 初始化路由绑定
-	bootstrap.SetupRoute(r)
+	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	err := r.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
